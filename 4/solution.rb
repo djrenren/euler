533bec84bae8aaf281bc0f71b159c3ede66286e0
jmrenner <john@jrenner.net>
2012-12-11 22:24:15 -0500

    Spell palindrome right

--- 4/solution.rb
+++ 4/solution.rb
@@ -1,4 +1,4 @@
-def palendrome?(num)
+def palindrome?(num)
   str = num.to_s
   (0...(str.length/2)).each do |i|
     if str[i] != str[-(i+1)]
@@ -13,7 +13,7 @@ mult = 0
 (100...999).each do |num1|
   (100...999).each do |num2|
     mult = num1 * num2
-    if mult > ans and palendrome? mult
+    if mult > ans and palindrome? mult
       ans = mult
     end
   end
